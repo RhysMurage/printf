a3008e70dc77fb82fb2bce2288dbf50e83366b64
RhysMurage <rhysmwangi96@gmail.com>
2022-04-17 16:30:59 -0400

    Initial commit

---
+++ 0-output_to_format.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+#include <stdarg.h>
+
+/**
+  *_printf- function that produces output according to a format
+  * @format: pointer to a constant character
+  *
+  * Return: number of characters printed
+  */
+
+int _printf(const char *format, ...)
+{
+	/* Total number of chars printed to stdout */
+	int char_count = 0;
+
+	/* Contains the list of arguments passed after format */
+	va_list ap;
+
+	/* Used to loop through all characters in format */
+	int i;
+
+	va_start(ap, format);
+
+	if (format == NULL)
+		return (char_count)
+}
