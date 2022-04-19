96bfe3adea2abba892a4c5d34b80e2738510f059
RhysMurage <rhysmwangi96@gmail.com>
2022-04-19 05:19:32 -0400

    Initial commit

--- _printf.c
+++ _printf.c
@@ -1,31 +1,29 @@
-#include <stdio.h>
-#include <unistd.h>
 #include "main.h"
 #include <stdarg.h>
+#include <stdlib.h>
 
 /**
-  *_printf- function that produces output according to a format
-  * @format: pointer to a constant character
-  *
-  * Return: number of characters printed
-  */
-
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
+ */
 int _printf(const char *format, ...)
 {
-	int(*pfunc)(va_list, flags_t *);
+	int (*pfunc)(va_list, flags_t *);
 	const char *p;
-	va_list args;
+	va_list arguments;
 	flags_t flags = {0, 0, 0};
 
 	register int count = 0;
 
-	va_start(args, format);
+	va_start(arguments, format);
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-
 	for (p = format; *p; p++)
 	{
 		if (*p == '%')
@@ -40,13 +38,12 @@ int _printf(const char *format, ...)
 				p++;
 			pfunc = get_print(*p);
 			count += (pfunc)
-				? pfunc(args, &flags)
+				? pfunc(arguments, &flags)
 				: _printf("%%%c", *p);
 		} else
 			count += _putchar(*p);
 	}
 	_putchar(-1);
-	va_end(args);
-
+	va_end(arguments);
 	return (count);
 }
