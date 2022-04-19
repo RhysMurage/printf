1c81878727f5c08263427663f7c3878c129b892e
RhysMurage <rhysmwangi96@gmail.com>
2022-04-19 05:28:37 -0400

    Fix error

--- main.h
+++ main.h
@@ -1,27 +1,24 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-/**
-  * struct flags- struct containing flags to "turn on"
-  * when a flag specifier is passed to _printf()
-  * @plus: flag for the '+' character
-  * @space: flag for the ' ' character
-  * @hash: flag for the '#' character
-  */
-typedef struct flags
-{
-	int plus;
-	int space;
-	int hash;
-} flags_t;
-
-
 int _putchar(char c);
 
-int _print(const char *format, ...);
+int _printf(const char *format, ...);
 
-int get_flag(char s, flags_t *f);
+int put_str(char *str);
 
-int (*get_print(char s))(va_list, flags_t *);
+int put_int(long n);
+
+int get_printing_func(char a, va_list *ap);
+
+int put_rot13(char *s);
+
+char *rot13(char *s);
+
+int _strlen(char *s);
+
+char *_strcpy(char *dest, char *src);
+
+int put_binary(unsigned int n);
 
 #endif /* _MAIN_H_ */
