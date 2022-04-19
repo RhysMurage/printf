7be990ab5fd23c1208795e74a30659e5bd5d8619
RhysMurage <rhysmwangi96@gmail.com>
2022-04-19 06:20:00 -0400

    Add helper function

--- _printf.c
+++ _printf.c
@@ -1,6 +1,31 @@
 #include "main.h"
 
 /**
+ * check_for_specifiers - checks if there is a valid format specifier
+ * @format: possible format specifier
+ *
+ * Return: pointer to valid function or NULL
+ */
+static int (*check_for_specifiers(const char *format))(va_list)
+{
+	unsigned int i;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{NULL,NULL}
+	};
+
+	for (i = 0; p[i].t != NULL; i++)
+	{
+		if (*(p[i].t) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
+}
+
+/**
  * _printf - prints anything
  * @format: list of argument types passed to the function
  *
