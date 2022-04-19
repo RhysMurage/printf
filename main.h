c8cd1ca92ca9ab498dcb1557f6e8dad1313c7ac5
RhysMurage <rhysmwangi96@gmail.com>
2022-04-19 10:06:13 -0400

    Fix error

--- main.h
+++ main.h
@@ -1,9 +1,9 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include "stdlib.h"
-#include "stdio.h"
-#include "stdarg.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
  * struct print - structure for printing various types
